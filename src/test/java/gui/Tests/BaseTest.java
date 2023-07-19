f17021b2c346e6187d39aede99e9a60ec73d2056
Matthias ZAX <matthias.zax@rbinternational.com>
2023-07-19 12:59:57 +0200

    add login Tests/BaseTest.java

--- src/test/java/gui/Tests/BaseTest.java
+++ src/test/java/gui/Tests/BaseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
     protected WebDriver driver;
 
@@ -12,6 +14,7 @@ public class BaseTest {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Program_Dev\\drivers\\chromedriver\\chromedriver_114.exe");
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterMethod
